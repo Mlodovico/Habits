3cd409a9587a0151bca907c88de2b2132f337794
Murilo Lodovico <37813008+Mlodovico@users.noreply.github.com>
2023-01-18 00:40:00 -0300

    01.00.01 new routes

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -35,10 +35,35 @@ export async function appRoutes(app: FastifyInstance) {
 
     const { date } = getDayParams.parse(req.query);
 
+    console.log("aqui", date);
+
+    const parsedDate = dayjs(date).startOf("day");
+    const weekDay = parsedDate.get("day");
+
     const possibleHabits = await prisma.habit.findMany({
       where: {
-        created_at: { lte: new Date() },
+        created_at: {
+          lte: date,
+        },
+        weekDays: {
+          some: { week_day: weekDay },
+        },
       },
     });
+
+    const day = await prisma.day.findUnique({
+      where: {
+        date: parsedDate.toDate(),
+      },
+      include: {
+        dayHabits: true,
+      },
+    });
+
+    const completedHabits = day?.dayHabits.map((dayHabit) => {
+      return dayHabit.habit_id;
+    });
+
+    return { possibleHabits, completedHabits };
   });
 }
